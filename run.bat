847e400471a359200121e98884b1c3d1c29d80ca
Apeksha <apelokhande@gmail.com>
2023-09-08 23:18:10 +0530

    First commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Apekshaa\eclipse-workspace\SeleniumTutorials
+mvn test
---
+++ src/test/java/Tutorials/NewTest.java
@@ -0,0 +1,20 @@
+package Tutorials;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() 
+  {
+	  System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/test/ajax.html");
+		driver.findElement(By.id("Yes")).click();
+		driver.findElement(By.id("buttoncheck")).click();
+
+  }
+}
